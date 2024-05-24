6de7efafdfe3cbb8a4c69daaae246226e3af79bf
ArthIJani <arth.jani2002@gmail.com>
2024-05-24 12:05:19 +0530

    home

--- home.py
+++ home.py
@@ -5,4 +5,5 @@ def home():
     st.markdown("""<h1 style='text-align: center;'>Welcome to AJ Programming Blogs</h1>
     <p style='text-align: center;'>Welcome to your one-stop shop for insightful content!</p>""",
                 unsafe_allow_html=True)
+    st.write("Checkout All Blogs in blogs section")
 
